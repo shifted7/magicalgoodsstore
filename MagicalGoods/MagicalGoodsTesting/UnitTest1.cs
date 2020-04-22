a93037ab8503d99643180994944a9b38f9c44577
andrewbc.dev@outlook.com <andrewbc.dev@outlook.com>
2020-04-21 22:03:58 -0700

    Added tests for Product properties and CRD (U not finished).

---
+++ MagicalGoods/MagicalGoodsTesting/UnitTest1.cs
@@ -0,0 +1,213 @@
+using MagicalGoods.Data;
+using MagicalGoods.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+using MagicalGoods;
+using System.Threading.Tasks;
+using MagicalGoods.Models.Services;
+using System.Collections.Generic;
+
+namespace MagicalGoodsTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestCanSetProductId()
+        {
+            Product testProduct = new Product
+            {
+                ID = 1,
+                Name = "Test",
+                Price = 10.00m,
+                Description = "Test Description",
+                Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+            };
+            testProduct.ID = 5;
+            Assert.Equal(5, testProduct.ID);
+        }
+
+        [Fact]
+        public void TestCanSetProductName()
+        {
+            Product testProduct = new Product
+            {
+                ID = 1,
+                Name = "Test",
+                Price = 10.00m,
+                Description = "Test Description",
+                Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+            };
+            testProduct.Name = "New name";
+            Assert.Equal("New name", testProduct.Name);
+        }
+
+        [Fact]
+        public void TestCanSetProductPrice()
+        {
+            Product testProduct = new Product
+            {
+                ID = 1,
+                Name = "Test",
+                Price = 10.00m,
+                Description = "Test Description",
+                Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+            };
+            testProduct.Price = 50.00m;
+            Assert.Equal(50.00m, testProduct.Price);
+        }
+
+        [Fact]
+        public void TestCanSetProductDescription()
+        {
+            Product testProduct = new Product
+            {
+                ID = 1,
+                Name = "Test",
+                Price = 10.00m,
+                Description = "Test Description",
+                Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+            };
+            testProduct.Description = "A new description.";
+            Assert.Equal("A new description.", testProduct.Description);
+        }
+
+        [Fact]
+        public void TestCanSetProductImage()
+        {
+            Product testProduct = new Product
+            {
+                ID = 1,
+                Name = "Test",
+                Price = 10.00m,
+                Description = "Test Description",
+                Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+            };
+            testProduct.Image = "https://image.shutterstock.com/image-illustration/full-hd-size-169-television-260nw-1448526302.jpg";
+            Assert.Equal("https://image.shutterstock.com/image-illustration/full-hd-size-169-television-260nw-1448526302.jpg", testProduct.Image);
+        }
+
+        [Fact]
+        public async Task TestCanAddProductToDatabase()
+        {
+            DbContextOptions<StoreDbContext> options = new DbContextOptionsBuilder<StoreDbContext>()
+                .UseInMemoryDatabase("TestCanAddProductToDatabase")
+                .Options;
+
+            // open the connection to the database
+            using (StoreDbContext context = new StoreDbContext(options))
+            {
+                ProductService ps = new ProductService(context);
+                Product testProduct = new Product
+                {
+                    ID = 1,
+                    Name = "Test",
+                    Price = 10.00m,
+                    Description = "Test Description",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+                };
+
+                var result = await ps.CreateProductAsync(testProduct);
+                var data = context.Products.Find(testProduct.ID);
+                Assert.Equal(result, data);
+            }
+        }
+
+        [Fact]
+        public async Task TestCanGetProductFromDatabase()
+        {
+            DbContextOptions<StoreDbContext> options = new DbContextOptionsBuilder<StoreDbContext>()
+                .UseInMemoryDatabase("TestCanGetProductFromDatabase")
+                .Options;
+
+            // open the connection to the database
+            using (StoreDbContext context = new StoreDbContext(options))
+            {
+                ProductService ps = new ProductService(context);
+                Product testProduct = new Product
+                {
+                    ID = 1,
+                    Name = "Test",
+                    Price = 10.00m,
+                    Description = "Test Description",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+                };
+
+                var addedProduct = await ps.CreateProductAsync(testProduct);
+                var getProduct = await ps.GetProductByIdAsync(testProduct.ID);
+                Assert.Equal(addedProduct, getProduct);
+            }
+        }
+
+        [Fact]
+        public async Task TestCanGetAllProductsFromDatabase()
+        {
+            DbContextOptions<StoreDbContext> options = new DbContextOptionsBuilder<StoreDbContext>()
+                .UseInMemoryDatabase("TestCanGetAllProductsFromDatabase")
+                .Options;
+
+            // open the connection to the database
+            using (StoreDbContext context = new StoreDbContext(options))
+            {
+                ProductService ps = new ProductService(context);
+                
+                Product testProductA = new Product
+                {
+                    ID = 1,
+                    Name = "TestA",
+                    Price = 10.00m,
+                    Description = "Test Description A",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+                };
+                Product testProductB = new Product
+                {
+                    ID = 2,
+                    Name = "TestB",
+                    Price = 20.00m,
+                    Description = "Test Description B",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+                };
+
+                List<Product> expectedProducts = new List<Product>();
+                expectedProducts.Add(testProductA);
+                expectedProducts.Add(testProductB);
+
+                await ps.CreateProductAsync(testProductA);
+                await ps.CreateProductAsync(testProductB);
+
+                List<Product> getAllProducts = await ps.GetAllProductsAsync();
+
+                Assert.Equal(expectedProducts, getAllProducts);
+            }
+        }
+
+        [Fact]
+        public async Task TestCanDeleteProductFromDatabase()
+        {
+            DbContextOptions<StoreDbContext> options = new DbContextOptionsBuilder<StoreDbContext>()
+                .UseInMemoryDatabase("TestCanDeleteProductFromDatabase")
+                .Options;
+
+            // open the connection to the database
+            using (StoreDbContext context = new StoreDbContext(options))
+            {
+                ProductService ps = new ProductService(context);
+                Product testProduct = new Product
+                {
+                    ID = 1,
+                    Name = "Test",
+                    Price = 10.00m,
+                    Description = "Test Description",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+                };
+
+                await ps.CreateProductAsync(testProduct);
+                await ps.RemoveProductByIdAsync(testProduct.ID);
+                var noProduct = await ps.GetProductByIdAsync(testProduct.ID);
+                Assert.Null(noProduct);
+            }
+        }
+
+        //TODO Test Can Update Product
+    }
+}
