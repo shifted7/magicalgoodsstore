b908b1d7c343c4acbe043cce92d63b1bbd85369f
Allyson Reyes <56554680+areyes986@users.noreply.github.com>
2020-04-29 13:00:09 -0700

    worked on the update and remove button, logic works

--- MagicalGoods/MagicalGoods/Migrations/StoreDb/20200429192809_addedtables.Designer.cs
+++ MagicalGoods/MagicalGoods/Migrations/StoreDb/20200429192809_addedtables.Designer.cs
@@ -3,14 +3,16 @@ using MagicalGoods.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MagicalGoods.Migrations.StoreDb
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200429192809_addedtables")]
+    partial class addedtables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
