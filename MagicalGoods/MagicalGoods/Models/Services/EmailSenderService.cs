0b286062477d3470c823ee3a4e2c79340d406f41
Allyson Reyes <56554680+areyes986@users.noreply.github.com>
2020-04-29 14:59:41 -0700

    created abilty to send receipt email to user

---
+++ MagicalGoods/MagicalGoods/Models/Services/EmailSenderService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SendGrid;
+using Microsoft.Extensions.Configuration;
+using SendGrid.Helpers.Mail;
+
+namespace MagicalGoods.Models.Services
+{
+    public class EmailSenderService : IEmailSender
+    {
+        private IConfiguration _configuration;
+
+        public EmailSenderService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            SendGridClient client = new SendGridClient(_configuration["SendGridKey"]);
+            SendGridMessage msg = new SendGridMessage();
+
+            msg.SetFrom("order-status@magicalgoods.com", "Andrew and Ally");
+            msg.AddTo(email);
+            msg.SetSubject(subject);
+            msg.AddContent(MimeType.Html, htmlMessage);
+
+            await client.SendEmailAsync(msg);
+
+        }
+    }
+}
--- MagicalGoods/MagicalGoods/Startup.cs
+++ MagicalGoods/MagicalGoods/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,6 +37,7 @@ namespace MagicalGoods
             services.AddTransient<IProductManager, ProductService>();
             services.AddTransient<ICartManager, CartService>();
             services.AddTransient<ICartProductManager, CartProductService>();
+            services.AddTransient<IEmailSender, EmailSenderService>();
 
             services.AddDbContext<ApplicationDbContext>(options =>
             {
