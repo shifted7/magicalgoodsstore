e684b3935e5f665c6ad6915b7443aaf9c029e8f0
andrewbc.dev@outlook.com <andrewbc.dev@outlook.com>
2020-05-04 17:18:50 -0700

    Started form input submission and model binding to checkout and payment pages.

--- MagicalGoods/MagicalGoods/Pages/Checkout/Index.cshtml.cs
+++ MagicalGoods/MagicalGoods/Pages/Checkout/Index.cshtml.cs
@@ -1,16 +1,82 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using MagicalGoods.Models;
+using MagicalGoods.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static MagicalGoods.Pages.Account.RegisterModel;
 
 namespace MagicalGoods.Pages.Checkout
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private UserManager<ApplicationUser> _userManager;
+        private ICartProductManager _cartProductService;
+        public List<CartProduct> CheckoutCartProducts { get; set; }
+        public decimal Total { get; set; }
+
+        [BindProperty]
+        public CheckoutInput UserData { get; set; }
+
+        public IndexModel(UserManager<ApplicationUser> userManager, ICartProductManager cartProductService)
         {
+            _userManager = userManager;
+            _cartProductService = cartProductService;
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            string userId = _userManager.GetUserId(User);
+            if (userId != null)
+            {
+                CheckoutCartProducts = await _cartProductService.GetAllProductsForCart(userId);
+                foreach (var cartProduct in CheckoutCartProducts)
+                {
+                    Total += cartProduct.Product.Price * cartProduct.Quantity;
+                }
+                return Page();
+            }
+            return RedirectToPage("/Account/Login");
+        }
+        public class CheckoutInput
+        {
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Shipping Address Line 1")]
+            public string ShippingAddressLine1 { get; set; }
+
+            [Display(Name = "Shipping Address Line 2")]
+            public string ShippingAddressLine2 { get; set; }
+
+            [Required]
+            [Display(Name ="City")]
+            public string City { get; set; }
+
+            [Required]
+            [Display(Name = "Zip Code")]
+            public string ZipCode { get; set; }
+
+            [Required]
+            [Display(Name = "StateOrProvince")]
+            public string StateOrProvince { get; set; }
+
+            [Required]
+            [Display(Name = "Country")]
+            public string Country { get; set; }
+
+            [Required]
+            [Display(Name = "Payment")]
+            public string Payment { get; set; }
         }
     }
 }
