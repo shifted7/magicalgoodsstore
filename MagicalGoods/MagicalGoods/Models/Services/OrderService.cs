a2c7b19fec303748a17940dc506c95b04c0afc9b
Allyson Reyes <56554680+areyes986@users.noreply.github.com>
2020-05-17 18:13:55 -0700

    created summary comments for the email sender, order, and payment service.

--- MagicalGoods/MagicalGoods/Models/Services/OrderService.cs
+++ MagicalGoods/MagicalGoods/Models/Services/OrderService.cs
@@ -16,12 +16,22 @@ namespace MagicalGoods.Models.Services
         {
             _storeContext = storeContext;
         }
+
+        /// <summary>
+        /// Creates an order to display on the admin orders page.
+        /// </summary>
+        /// <param name="order">takes in an order</param>
         public async Task CreateOrder(Order order)
         {
             _storeContext.Add(order);
             await _storeContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// creates ability for the admin to delete the order 
+        /// </summary>
+        /// <param name="id">the id of the order that wants to be deleted</param>
+        /// <returns></returns>
         public async Task DeleteOrder(int id)
         {
             Order order = await _storeContext.Orders.FindAsync(id);
@@ -29,6 +39,10 @@ namespace MagicalGoods.Models.Services
             await _storeContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Gets all the orders that were created to display to the admin 
+        /// </summary>
+        /// <returns>the details of the order</returns>
         public async Task<List<Order>> GetAllOrders()
         {
             List<Order> orders = await _storeContext.Orders.Include(order => order.Cart)
@@ -38,6 +52,11 @@ namespace MagicalGoods.Models.Services
             return orders;
         }
 
+        /// <summary>
+        /// Gets all the orders to display as well as including the cartproducts with the products and displays the list
+        /// </summary>
+        /// <param name="id">the order id</param>
+        /// <returns>the order details</returns>
         public async Task<Order> GetOrderByID(int id)
         {
             var result = await _storeContext.Orders.Where(order=> order.ID == id)
--- MagicalGoods/MagicalGoods/Models/Services/PaymentService.cs
+++ MagicalGoods/MagicalGoods/Models/Services/PaymentService.cs
@@ -21,6 +21,11 @@ namespace MagicalGoods.Models.Services
             _config = configuration;
         }
 
+        /// <summary>
+        /// controllers.Base for authorize.net. Sets up the environment needed for authorize.net to run. We had also created/ set up a credit card, sets up our transactions
+        /// </summary>
+        /// <param name="checkoutInput">The users input from the checkout form</param>
+        /// <returns></returns>
         public string Run(CheckoutInput checkoutInput)
         {
             ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
@@ -31,12 +36,14 @@ namespace MagicalGoods.Models.Services
                 Item = _config["AN-TransactionKey"]
             };
 
+            // creates the credits cards and passes in the type of card the user chooses to use. Either it be mastercard, visa, or american express
             var creditCard = new creditCardType
             {
                 cardNumber = checkoutInput.Payment,
                 expirationDate = "1021",
                 cardCode = "102"
             };
+
 
             customerAddressType billingAddress = GetAddress(checkoutInput);
 
@@ -58,6 +65,7 @@ namespace MagicalGoods.Models.Services
 
             var response = controller.GetApiResponse();
 
+            // if the response fails, the user will see an error that the payment has failed to recieve a response
             if(response != null)
             {
                 if (response.messages.resultCode == messageTypeEnum.Ok)
@@ -69,6 +77,11 @@ namespace MagicalGoods.Models.Services
             return "Failed to recieve response";
         }
 
+        /// <summary>
+        /// Brings in what user types into the entry to create the user's address
+        /// </summary>
+        /// <param name="checkoutInput">the input of the user from the form</param>
+        /// <returns>the address</returns>
         public customerAddressType GetAddress(CheckoutInput checkoutInput)
         {
             customerAddressType address = new customerAddressType
